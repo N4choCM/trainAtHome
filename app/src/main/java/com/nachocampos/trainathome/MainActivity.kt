ff44118985772812d3f72ffadcf5f2d4b5a62d60
N4choCM <nachocamposmarti@gmail.com>
2022-11-01 21:44:16 +0100

    Initial commit

---
+++ app/src/main/java/com/nachocampos/trainathome/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.nachocampos.trainathome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.nachocampos.trainathome.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will start the exercise.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+
+}
